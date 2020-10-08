d5d18550823a911f2b3fe6fe257223a5652625b1
Redvik <redvik80@mail.ru>
2020-10-08 18:51:30 +0300

    fix

--- lab1.js
+++ lab1.js
@@ -1,53 +1,36 @@
-var Product = /** @class */ (function () {
-    function Product(id, name) {
+class Product {
+    constructor(id, name) {
         this.id = id;
         this.name = name;
     }
-    Object.defineProperty(Product.prototype, "id", {
-        get: function () {
-            return this._id;
-        },
-        set: function (value) {
-            this._id = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Product.prototype, "name", {
-        get: function () {
-            return this._name;
-        },
-        set: function (value) {
-            this._name = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Product.prototype, "rest", {
-        get: function () {
-            return this._rest;
-        },
-        set: function (value) {
-            this._rest = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Product.prototype, "price", {
-        get: function () {
-            return this._price;
-        },
-        set: function (value) {
-            this._price = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Product.prototype.addToCart = function (cart, quantity) {
-        cart.unshift({ id: this.id, quantity: quantity });
-    };
-    Product.prototype.addToFavorites = function (favorites) {
+    get id() {
+        return this._id;
+    }
+    set id(value) {
+        this._id = value;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+    get rest() {
+        return this._rest;
+    }
+    set rest(value) {
+        this._rest = value;
+    }
+    get price() {
+        return this._price;
+    }
+    set price(value) {
+        this._price = value;
+    }
+    addToCart(cart, quantity) {
+        cart.unshift({ id: this.id, quantity });
+    }
+    addToFavorites(favorites) {
         favorites.unshift(this.id);
-    };
-    return Product;
-}());
+    }
+}
